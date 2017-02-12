ff1a81cfd3dc70c9655620c3bdb8b7b79c5b5f38
juwaini <juwaini@gmail.com>
2017-02-12 22:36:40 +0800

    Create boilerplate for Problem 2

---
+++ problem_0002.py
@@ -0,0 +1,17 @@
+def fibo_number(term):
+    if term == 0:
+        return 1
+    elif term == 1:
+        return 2
+    else:
+        return
+
+
+def is_even(x):
+    if x % 2 == 0:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    total = 0
