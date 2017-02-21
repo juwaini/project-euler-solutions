4666fe564263ed27c8faff7c6090b74ba61c8e74
juwaini <juwaini@gmail.com>
2017-02-21 23:49:00 +0800

    Cater for 2 digit case in problem 4

--- problem_0004.py
+++ problem_0004.py
@@ -13,8 +13,16 @@ def generate_product_as_string(x, y):
 
 if __name__ == '__main__':
     palindrome_list = []
-    for i in range(100, 999):
-        for j in range(100, 999):
+    for i in range(10, 100):
+        for j in range(10, 100):
+            product = str(i*j)
+            if is_palindrome(product):
+                palindrome_list.append(i*j)
+    print("Largest palindrome made from product of 2 digit number: %d" % max(palindrome_list))
+
+    palindrome_list = []
+    for i in range(100, 1000):
+        for j in range(100, 1000):
             product = str(i*j)
             if is_palindrome(product):
                 palindrome_list.append(i*j)
