0f006b5608844b8110dd5f275075109bbfdda2ec
juwaini <juwaini@gmail.com>
2017-02-21 23:40:08 +0800

    Solve problem 4

---
+++ problem_0004.py
@@ -0,0 +1,21 @@
+import math
+
+
+def is_palindrome(s):
+    for x in range(0, math.floor(len(s)/2)):
+        if s[x] != s[-(x+1)]:
+            return False
+    return True
+
+
+def generate_product_as_string(x, y):
+    return str(x*y)
+
+if __name__ == '__main__':
+    palindrome_list = []
+    for i in range(100, 999):
+        for j in range(100, 999):
+            product = str(i*j)
+            if is_palindrome(product):
+                palindrome_list.append(i*j)
+    print("Largest palindrome made from product of 3 digit number: %d" % max(palindrome_list))
