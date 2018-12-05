bca76462baaf9c2c45757be04449e2f8489b88f6
Juwaini <juwaini@gmail.com>
2018-12-05 23:30:47 +0800

    solve problem 35

--- problem_0035.py
+++ problem_0035.py
@@ -1,16 +1,52 @@
 from utils.prime import is_prime
-from itertools import permutations
+
+
+def generate_circular(n):
+    number_of_circular = len(str(n))
+    circulars = [n]
+
+    for i in range(number_of_circular - 1):
+        str_n = list(str(circulars[i]))
+        if ('2' in str_n) or ('5' in str_n) or ('0' in str_n):
+            return []
+
+        tmp = str_n[0]
+        for j in range(len(str_n)):
+            if j == len(str_n) - 1:
+                str_n[j] = tmp
+            else:
+                str_n[j] = str_n[j+1]
+
+        circulars.append(int(''.join(str_n)))
+
+    return circulars
 
 
 def is_circular_prime(n):
-    str_n = list(str(n))
-    perm = set(list(permutations(str_n)))
-    for p in perm:
-        if not is_prime(int(''.join(p))):
-            print(int(''.join(p)))
+    circs = generate_circular(n)
+
+    if not circs:
+        return False
+
+    for p in circs:
+        if not is_prime(p):
             return False
+
+    # print(circs)
     return True
 
 
 if __name__ == '__main__':
     assert is_circular_prime(197)
+
+    cp = set()
+    for i in range(2, 101):
+        if is_circular_prime(i):
+            cp.add(i)
+    print(sorted(cp), len(cp))
+
+    cp = set()
+    for i in range(2, 1_000_001):
+        if is_circular_prime(i):
+            cp.add(i)
+    print(sorted(cp), len(cp))
