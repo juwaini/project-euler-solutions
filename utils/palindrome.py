a9a02b9139e12b9f8176c75c3964ac4a81e44b7a
Juwaini <juwaini@gmail.com>
2019-09-12 17:18:34 +0800

    implement another way to check palindrome

--- utils/palindrome.py
+++ utils/palindrome.py
@@ -2,14 +2,20 @@ def is_palindrome(s):
     if type(s) != str:
         s = str(s)
 
-    for i in range(0, (len(s)//2) + 1):
-        if s[i] != s[-(i + 1)]:
-            return False
+    # for i in range(0, (len(s)//2) + 1):
+    #     if s[i] != s[-(i + 1)]:
+    #         return False
+    # return True
 
-    return True
+    while s[0] == s[-1]:
+        if len(s) == 1 or s == '':
+            return True
+        else:
+            s = s[1:-1]
+    return False
 
 
 if __name__ == '__main__':
-    assert is_palindrome(131)
-    assert is_palindrome(100010001)
-    assert is_palindrome('drawkward')
+    assert is_palindrome(131) == True
+    assert is_palindrome(100010001) == True
+    assert is_palindrome('drawkward') == True
